fe8ada69faa49d714f58a0644c046f5b0da8de64
ayrton <ayrton.parkinson1@gmail.com>
2024-05-13 08:08:11 -0400

    Fix spec generators

--- application_template.rb
+++ application_template.rb
@@ -9,9 +9,9 @@ end
 initializer 'generators.rb', <<-CODE
   Rails.application.config.generators do |g|
     g.test_framework :rspec,
-      fixtures:
-      view_specs:
-      helper_specs:
+      fixtures:         false,
+      view_specs:       false,
+      helper_specs:     false,
       routing_specs:    false,
       request_specs:    false,
       controller_specs: false
